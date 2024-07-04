1999aedaf4340db408eb6e0494634f74054af74d
PAUL BOURY <paulboury17@gmail.com>
2024-07-04 12:34:44 +0200

    error404 not found is done

--- dev-react/src/pages/ErrorPage404/ErrorPage404.jsx
+++ dev-react/src/pages/ErrorPage404/ErrorPage404.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Logo404 from "../../assets/LOGO_404.svg";
+import HeaderBlue from "../../components/HeaderBlue/HeaderBlue";
+import Footer from "../../components/Footer/Footer"
+import "./ErrorPage404.css";
 
 function ErrorPage404() {
   return (
-    <div>ErrorPage404</div>
-  )
+    <>
+      <main className="main404">
+      <HeaderBlue />
+      <figure className="figure404">
+        <img src={Logo404} alt="Logo404" className="image404" />
+        <figcaption  className="caption404">
+          Oops ! Cette idée est en cours d'élaboration.. Erreur 404
+        </figcaption>
+      </figure>
+      <Footer />
+      </main>
+    </>
+  );
 }
 
-export default ErrorPage404
+export default ErrorPage404;
