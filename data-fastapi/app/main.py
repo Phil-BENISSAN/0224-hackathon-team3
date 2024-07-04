38aa4c0c6a30903bfcf6354a47a9d9e651189495
AnnBoRo <annagaelrouault@cegetel.net>
2024-07-04 17:46:31 +0200

    ajout des hommes dans la bdd et modif de la route get ideas

--- data-fastapi/app/main.py
+++ data-fastapi/app/main.py
@@ -41,7 +41,7 @@ def fn_fast_api():
 @app.get('/personnages')
 def fn_fast_api():
     
-    df_personnages = pd.read_csv("app/df_personnages.csv")
+    df_personnages = pd.read_csv("app/df_personnes_celebres.csv")
     
     dict_from_df = df_personnages.to_dict('records')
     
@@ -65,9 +65,9 @@ async def create_item(item: Item):
 
 
 @app.get("/ideas")
-async def give_item(item: Item):
+async def give_item():
     
-    return "OK ca fonctionne"
+    return "OK ça fonctionne"
 
 #------------------------------------------------------
 
