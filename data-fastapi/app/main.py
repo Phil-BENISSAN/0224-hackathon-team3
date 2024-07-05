616b037f28e1b7438d0b9389b3985c44b9c20073
AnnBoRo <annagaelrouault@cegetel.net>
2024-07-05 09:25:29 +0200

    fast api derniere version

--- data-fastapi/app/main.py
+++ data-fastapi/app/main.py
@@ -10,6 +10,10 @@ class Item(BaseModel):
     title : str
     idea  : str
      
+store_ideas = []
+
+df = pd.read_csv("app/ideas_box.csv")
+store_ideas = df.to_dict('records')
   
 # -------------------------------------------------   CONFIGURATION CORS   ------------------------------------------------------
 
@@ -25,10 +29,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
+# Root 
 @app.get('/')
-def fn_fast_api():
+def home():
 
 # -------  INSERER VOTRE CODE ICI -----------------
     
@@ -37,12 +40,11 @@ def fn_fast_api():
 # ---------------- FIN DE TON CODE ----------------
 #__________________________________________________
 
-
+# Pour recuperation des donnees sur les personnes celebres
 @app.get('/personnages')
-def fn_fast_api():
+def get_personnages():
     
     df_personnages = pd.read_csv("app/df_personnes_celebres.csv")
-    
     dict_from_df = df_personnages.to_dict('records')
     
 # -------  INSERER VOTRE CODE ICI -----------------
@@ -53,24 +55,38 @@ def fn_fast_api():
 # ---------------- FIN DE TON CODE ----------------
 #__________________________________________________
 
-
-
+# Pour remplir la boite à idees
 @app.post("/ideas")
 async def create_item(item: Item):
+    global store_ideas    
+    # df = pd.read_csv("app/ideas_box.csv")
     
-    return {"item": item}
+    # store_ideas = df.to_dict('records')
+    
+    store_ideas.append(item)    
+        
+    # df_out = pd.DataFrame(store_ideas)
+    # df.to_csv("app/ideas_box.cvs", index=False)
+    
+    # return store_ideas    
+    return store_ideas
 
 #------------------------------------------------------
 
-
-
+# Pour recuperer le contenu de la boite à idees
 @app.get("/ideas")
 async def give_item():
-    
-    return "OK ça fonctionne"
+    global store_ideas 
+    # df = pd.read_csv("app/ideas_box.csv")
+    # store_ideas = df.to_dict('records')
+        
+    return store_ideas
 
 #------------------------------------------------------
 
+
+
+# Lancement de l application
 if __name__ == '__main__':
     uvicorn.run(app, port=8000, host='0.0.0.0')
 
